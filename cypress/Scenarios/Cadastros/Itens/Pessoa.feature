072dbc447fdca945c3277fe5256ce66b3fe988ba
Victor_Galeazzi <victor.galeazzi@viasoft.com.br>
2024-03-11 16:32:56 -0300

    Método pegando label e buscando propriedade for para fazer o preenchimento do campo pelo ID

--- cypress/Scenarios/Cadastros/Itens/Pessoa.feature
+++ cypress/Scenarios/Cadastros/Itens/Pessoa.feature
@@ -9,5 +9,8 @@ Funcionalidade: Pessoa
 
     Cenário: Cadastrando Nova Pessoa
         Quando clico no botão 'Novo' no Formulário de Pessoa
-        E seleciono o campo 'País' com valor 'ALEMANHA' na tela de Pessoa
+        E seleciono o campo 'País' com valor 'ANGOLA' na tela de Pessoa
         E preencho o campo 'Nome' com o valor 'Teste Automatizado' na tela de Pessoa
+        E preencho o campo 'Identificação' com o valor 'Teste Automatizado' na tela de Pessoa
+        E preencho o campo 'Data de Nascimento' com o valor '20/04/2024' na tela de Pessoa
+        E seleciono o campo 'Atribuição' com valor 'Cliente' na tela de Pessoa
--- cypress/Scripts/Actions/PessoaActions.js
+++ cypress/Scripts/Actions/PessoaActions.js
@@ -7,62 +7,41 @@ class PessoaActions {
     selecionarOpcao(label, valor) {
         cy.selectOption(label, valor)
     }
-    
+
     preencherCampo(param, texto) {
 
-    //     let fodase = cy.get('.p-card-content').find('.form-group')
-    //         .contains(param).its('[0].htmlFor').then((value) => {
-    //             fodase = value; // Armazena o valor da propriedade htmlFor
-    //           });
-        
-    //         cy.get(`#`+value).debug()
-    // }
-
-    cy.get('.p-card-content').find('.form-group').contains(param)
-    cy.get('input[name="teacher[0].number"]').type('8000-1612023')
-
-    cy.get('.p-card-content').find('.form-group').invoke('attr', 'innerHtml').then((param) => 
-
         cy.get('.p-card-content')
-        .find('.form-group')
-        .find('label')
-        .invoke('attr','for')
-        .should('eq', 'nome') // Verifica se o elemento label possui o atributo 'for'
-        .then(($el) => {
-            cy.get('#nome').type('123')
-            // cy.wrap($el).should('have.attr', 'for', 'nome'); 
-        })
-        )
+            .find('.form-group')
+            .find('label')
+            .contains(param)
+            .invoke('attr', 'for')
+            .then((forValue) => {
+                // Verifica se o atributo 'for' existe e não é nulo
+                if (forValue) {
+                    // Usa o valor do atributo 'for' para selecionar o elemento pelo ID
+                    cy.get(`#${forValue}`).type(texto);
+                } else {
+                    // Caso o atributo 'for' não exista ou seja nulo
+                    cy.log('O atributo "for" não foi encontrado ou é nulo');
+                }
+            });
     }
-
-    // cy.get('.p-card-content')
-    // .find('.form-group')
-    // .contains(param)
-    // .should('exist') // Verifica se o elemento label existe
-    // .then(($el) => {
-    //     cy.wrap($el).should('have.attr', 'for', 'nome'); // Verifica se o atributo 'for' tem o valor 'nome'
-    // });
-  
-  
-    // preencherCampo(param, texto) {
-    //     cy.get('.p-card-content').find('.form-group')
-    //         .find('#nome')
 
     getRegistro() {
         const id = Cypress.env('idGlobal');
-    
+
         cy.get('.table-body-line')
             .find('.ellipsis')
             .contains(id)
             .click();
-       }
-        
+    }
+
     salvaId() {
-        cy.wait(500) 
+        cy.wait(500)
         cy.get('header-form').find('.codigo-format').then(($text) => {
             let txt = $text.text()
             txt = Number(txt).toString();
-        
+
             cy.saveId(txt);
             cy.log(txt);
         });
